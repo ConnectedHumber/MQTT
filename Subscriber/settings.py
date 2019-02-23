6f7a793282312ddd9c79cbf33f3de482e9812c75
Brian <brian.n.norman@gmail.com>
2019-02-23 19:37:23 +0000

    Updated for dbLoader.py

--- Subscriber/settings.py
+++ Subscriber/settings.py
@@ -1,31 +1,37 @@
 # settings.py
 #
 # Author: 	Brian Norman
-# Date: 	19th Feb 2019
+# Date: 	23rd Feb 2019
 #
-# place this file in the same folder as chDataLoad_JSON_Vx.xx.py
+# place this file in the same folder as dbLoader.py
 #
 # this file should only be readable by root(sudo chmod 0644 should do it)
 #
 #
-# it provides the passwords etc needed for chDataLoad_JSON_Vx.xx.py to
+# it provides the passwords etc needed for dbLoader.py to
 # connect to an MQTT broker and local database
 #
-# if mqttClientUser is set to None
+# if mqttClientUser is set to None authentication is not used
+# but the broker must accept conenctions without it
 
 
 # MQTT settings
 mqttBroker = 'mqtt.connectedhumber.org'
-mqttClientUser = "mqttUsername"
-mqttClientPassword = "mqttPassword"
+mqttClientUser = "mqttClientUser"
+mqttClientPassword = "mqttClientPassword"
 mqttTopic = "airquality/data"
-mqttConnectTimeout=20 # seconds to wait for on_connect at startup
-mqttKeepAlive=60		# ping will be sent if no messages have for this time
+mqttConnectTimeout=20	# only checked on startup
+mqttKeepAlive=60		# client pings server if no messages have been sent in this time period.
+
 
 # database settings
 dbHost="127.0.0.1"
-dbUser="dbUsername"
+dbUser="dbUser"
 dbPassword="dbPassword"
 dbName="aq_db"
 
-logFile="/var.log/aq_db.log"
+# LOGGING
+logFile="/var/log/aq_db.log"
+
+MAX_MESSAGE_NUMBER=9999
+MAX_JOBS=100
